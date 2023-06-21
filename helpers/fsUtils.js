ff2d4c6ec1c2272e570c9e5dc78660881c433b57
AMillsy <00adammills00@gmail.com>
2023-06-21 12:20:17 +0100

    writing Data to file with a helper Fsutils class

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -4,9 +4,41 @@ const { response } = require("../routes");
 const res = require("express/lib/response");
 
 class fsUtils {
+  // Make into a promise so that you can do .thens
   readFromFile = util.promisify(fs.readFile);
 
-  
+  writeToFile(content, fileLocation) {
+    const data = [];
+    this.readFromFile(fileLocation).then((response) => {
+      data.push(JSON.parse(response));
+      data.push(content);
+      this.#writeDatatoFile(data, fileLocation);
+    });
+  }
+
+  deleteDataFromFile(id, fileLocation) {
+    const data = [];
+    this.readFromFile(fileLocation).then((response) => {
+      data.push(JSON.parse(response));
+      
+      const returnData = data.map(note =>{
+        if(note.id === id){
+          return note;
+        }
+        else{
+          
+        };
+      });
+    });
+  }
+
+  #writeDatatoFile(data, fileLocation) {
+    fs.writeFile(fileLocation, JSON.stringify(data, null, 4), (err) => {
+      return err
+        ? console.error(err)
+        : console.log("Successfully added data", data);
+    });
+  }
 }
 
 module.exports = { fsUtils };
